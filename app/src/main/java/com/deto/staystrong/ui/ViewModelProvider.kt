247fdef72cf91d646301c5a9915f5d9e146687fb
detossj <jorgerubilarssj@gmail.com>
2025-06-18 20:56:07 -0400

    added fun CreationExtras

--- app/src/main/java/com/deto/staystrong/ui/ViewModelProvider.kt
+++ app/src/main/java/com/deto/staystrong/ui/ViewModelProvider.kt
@@ -1,9 +1,15 @@
 package com.deto.staystrong.ui
 
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.deto.staystrong.StayStrong
 
 object ViewModelProvider {
     val Factory = viewModelFactory {
 
     }
 }
+
+fun CreationExtras.App(): StayStrong =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as StayStrong)
