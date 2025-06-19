4e3a96abe4da924bdec1b8fb011cd3a1ad508498
detossj <jorgerubilarssj@gmail.com>
2025-06-19 00:06:22 -0400

    added objects and function Navigation

--- app/src/main/java/com/deto/staystrong/NavController.kt
+++ app/src/main/java/com/deto/staystrong/NavController.kt
@@ -1,2 +1,46 @@
 package com.deto.staystrong
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.deto.staystrong.ui.auth.AuthManager
+import com.deto.staystrong.ui.auth.LoginScreen
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object AuthManager
+
+@Serializable
+object Login
+
+@Serializable
+object Register
+
+@Serializable
+object Exercise
+
+
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = AuthManager) {
+
+        composable<AuthManager> {
+            AuthManager(navController = navController)
+        }
+        composable<Login> {
+            LoginScreen(navController = navController)
+        }
+        composable<Register> {
+
+        }
+        composable<Exercise> {
+
+        }
+    }
+}
