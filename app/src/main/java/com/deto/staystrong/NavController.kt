54d6e5ca9e2e49c1747da897eb81480362a57612
detossj <jorgerubilarssj@gmail.com>
2025-06-19 18:42:03 -0400

    added screens

--- app/src/main/java/com/deto/staystrong/NavController.kt
+++ app/src/main/java/com/deto/staystrong/NavController.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.deto.staystrong.ui.auth.AuthManager
 import com.deto.staystrong.ui.auth.LoginScreen
+import com.deto.staystrong.ui.auth.RegisterScreen
 import kotlinx.serialization.Serializable
 
 
@@ -34,10 +35,10 @@ fun Navigation() {
             AuthManager(navController = navController)
         }
         composable<Login> {
-            LoginScreen()
+            LoginScreen(navController = navController)
         }
         composable<Register> {
-
+            RegisterScreen(navController = navController)
         }
         composable<Exercise> {
 
