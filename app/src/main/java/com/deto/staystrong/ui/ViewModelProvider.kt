05ecdc2709af0613425d1fb7ee90fd9cfed250be
detossj <jorgerubilarssj@gmail.com>
2025-06-19 00:06:46 -0400

    added AuthViewModel

--- app/src/main/java/com/deto/staystrong/ui/ViewModelProvider.kt
+++ app/src/main/java/com/deto/staystrong/ui/ViewModelProvider.kt
@@ -2,12 +2,17 @@ package com.deto.staystrong.ui
 
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.deto.staystrong.StayStrong
+import com.deto.staystrong.ui.auth.AuthViewModel
 
-object ViewModelProvider {
+object AppViewModelProvider {
     val Factory = viewModelFactory {
 
+        initializer {
+            AuthViewModel(App().container.authUiState, App().baseContext)
+        }
     }
 }
 
