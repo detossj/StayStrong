9e701c3188b4f9d3a89baee07f71473da814fbd9
detossj <jorgerubilarssj@gmail.com>
2025-06-18 20:55:43 -0400

    added class StayStrong

--- app/src/main/java/com/deto/staystrong/AppContainer.kt
+++ app/src/main/java/com/deto/staystrong/AppContainer.kt
@@ -1,2 +1,14 @@
 package com.deto.staystrong
 
+import android.app.Application
+import com.deto.staystrong.data.remote.AppContainer
+import com.deto.staystrong.data.remote.AppDataContainer
+
+class StayStrong : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppDataContainer(this)
+    }
+}
