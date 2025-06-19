f63e64b25f935c34bf3a6c3e7c6d616dde07629f
detossj <jorgerubilarssj@gmail.com>
2025-06-19 00:04:22 -0400

    added class AuthViewModel

--- app/src/main/java/com/deto/staystrong/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/deto/staystrong/ui/auth/AuthViewModel.kt
@@ -1,2 +1,116 @@
 package com.deto.staystrong.ui.auth
 
+import android.content.Context
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deto.staystrong.data.remote.TokenManager
+import kotlinx.coroutines.launch
+import com.deto.staystrong.data.remote.services.AuthService
+import com.deto.staystrong.model.LoginRequest
+import com.deto.staystrong.model.RegisterRequest
+
+sealed class AuthUiState {
+    object Idle : AuthUiState()
+    object Loading : AuthUiState()
+    data class loggedIn(var logged: Boolean) : AuthUiState()
+    data class Success(val token: String) : AuthUiState()
+    data class Error(val message: String) : AuthUiState()
+
+}
+
+class AuthViewModel(
+    private val authService: AuthService,
+    private val context: Context
+) : ViewModel() {
+
+    var authState: AuthUiState by mutableStateOf(AuthUiState.Idle)
+        private set
+
+    init {
+        isLoggedIn()
+    }
+    fun login(email: String, password: String) {
+
+        viewModelScope.launch {
+
+            authState = AuthUiState.Loading
+
+            try {
+
+                val response = authService.login(LoginRequest(email, password))
+                TokenManager.saveToken(context, response.token)
+                authState = AuthUiState.Success(response.token)
+
+            } catch (e: Exception) {
+
+                authState = AuthUiState.Error(e.message ?: "Login failed")
+
+            }
+        }
+    }
+
+    fun register(name: String, email: String, password: String) {
+
+        viewModelScope.launch {
+
+            authState = AuthUiState.Loading
+
+            try {
+
+                val response = authService.register(RegisterRequest( name, email, password, password ))
+                TokenManager.saveToken(context, response.token)
+                authState = AuthUiState.Success(response.token)
+
+
+            } catch (e: Exception) {
+
+                authState = AuthUiState.Error(e.message ?: "Login failed")
+
+            }
+        }
+    }
+
+    fun logout() {
+
+
+        viewModelScope.launch {
+            try {
+                val token = TokenManager.getToken(context)
+                if (token != null) {
+                    authService.logout("Bearer $token")
+                }
+                TokenManager.clearToken(context)
+                authState = AuthUiState.Idle
+            } catch (e: Exception) {
+                authState = AuthUiState.Error("")
+            }
+
+        }
+    }
+
+    private fun isLoggedIn() {
+        viewModelScope.launch {
+            authState = AuthUiState.Loading
+            try {
+                val token = TokenManager.getToken(context)
+                if (token != null) {
+                    // Opción rápida: Confiar en el token local ( funciona para login persistente )
+                    authState = AuthUiState.loggedIn(true)
+
+                    // Opción segura: Validar token con el backend
+                    // val response = authService.getUser() // Llamada a /profile
+                    // authState = AuthUiState.loggedIn(true)
+                } else {
+                    authState = AuthUiState.loggedIn(false)
+                }
+            } catch (e: Exception) {
+                authState = AuthUiState.loggedIn(false)
+            }
+        }
+    }
+
+
+}
