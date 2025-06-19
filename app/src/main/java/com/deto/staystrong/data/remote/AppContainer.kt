29cc4377b09504e097234fe995b78cb2567a7017
detossj <jorgerubilarssj@gmail.com>
2025-06-19 00:03:16 -0400

    added AuthService

--- app/src/main/java/com/deto/staystrong/data/remote/AppContainer.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/AppContainer.kt
@@ -1,11 +1,16 @@
 package com.deto.staystrong.data.remote
 
 import android.content.Context
+import com.deto.staystrong.data.remote.services.AuthService
 
 interface AppContainer {
-
+    val authUiState: AuthService
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
+
+    override val authUiState: AuthService by lazy {
+        ApiClient.create(context).create(AuthService::class.java)
+    }
 
 }
