84025f3273ee889f5b0cf4aa3ef3b82b0e7751a9
detossj <jorgerubilarssj@gmail.com>
2025-06-19 00:03:39 -0400

    added function AuthManager

--- app/src/main/java/com/deto/staystrong/ui/auth/AuthManager.kt
+++ app/src/main/java/com/deto/staystrong/ui/auth/AuthManager.kt
@@ -1,2 +1,45 @@
 package com.deto.staystrong.ui.auth
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.deto.staystrong.AuthManager
+import com.deto.staystrong.Exercise
+import com.deto.staystrong.Login
+import com.deto.staystrong.ui.AppViewModelProvider
+
+@Composable
+fun AuthManager(navController: NavHostController, viewModel: AuthViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
+    val authState = viewModel.authState
+
+    LaunchedEffect(authState) {
+        when (authState) {
+            is AuthUiState.loggedIn -> {
+                if (authState.logged) {
+                    navController.navigate(Exercise) {
+                        popUpTo(AuthManager) {
+                            inclusive = true
+                        } // Quitar la pantalla de la pila de navegación
+                    }
+                }
+                if (!authState.logged) {
+                    navController.navigate(Login) {
+                        popUpTo(AuthManager) {
+                            inclusive = true
+                        } // Quitar la pantalla de la pila de navegación
+                    }
+                }
+            }
+            else -> {}
+        }
+    }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
+}
