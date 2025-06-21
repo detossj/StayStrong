06e5203a2701ab548ce94b584b00c8e255ffa858
detossj <jorgerubilarssj@gmail.com>
2025-06-21 18:19:53 -0400

    changed network domain

--- app/src/main/java/com/deto/staystrong/MainActivity.kt
+++ app/src/main/java/com/deto/staystrong/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.deto.staystrong
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.deto.staystrong.ui.theme.StayStrongTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
