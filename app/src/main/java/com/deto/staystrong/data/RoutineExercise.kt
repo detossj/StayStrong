62546f27c44c44f1b2a0e8f7d7220949496aa2cd
deto <jorgerubilarssj@gmail.com>
2025-06-20 20:53:17 -0400

    changed

--- app/src/main/java/com/deto/staystrong/data/RoutineExercise.kt
+++ app/src/main/java/com/deto/staystrong/data/RoutineExercise.kt
@@ -4,9 +4,9 @@ import androidx.room.PrimaryKey
 
 data class RoutineExercise(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    val routine_id: Int = 0,
-    val exercise_id: Int = 0,
-    val order: Int = 0,
+    val id: Int,
+    val routine_id: Int,
+    val exercise_id: Int,
+    val order: Int,
 
 )
