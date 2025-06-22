b7492927d367f805d7810b68721de0b54cce8e8a
detossj <jorgerubilarssj@gmail.com>
2025-06-22 00:31:50 -0400

    added exerciseApiService

--- app/src/main/java/com/deto/staystrong/data/AppContainer.kt
+++ app/src/main/java/com/deto/staystrong/data/AppContainer.kt
@@ -3,11 +3,13 @@ package com.deto.staystrong.data
 import android.content.Context
 import com.deto.staystrong.data.remote.ApiClient
 import com.deto.staystrong.data.remote.services.AuthService
+import com.deto.staystrong.data.remote.services.ExerciseService
 import com.deto.staystrong.data.remote.services.RoutineService
 
 interface AppContainer {
     val authApiService: AuthService
     val routineApiService: RoutineService
+    val exerciseApiService: ExerciseService
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
@@ -18,6 +20,10 @@ class AppDataContainer(private val context: Context) : AppContainer {
 
     override val routineApiService: RoutineService by lazy {
         ApiClient.create(context).create(RoutineService::class.java)
+    }
+
+    override val exerciseApiService: ExerciseService by lazy {
+        ApiClient.create(context).create(ExerciseService::class.java)
     }
 
 }
