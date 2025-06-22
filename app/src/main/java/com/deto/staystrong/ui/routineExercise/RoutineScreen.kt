6f34d98b9c173dee70dbe535481f92fecd5224b5
detossj <jorgerubilarssj@gmail.com>
2025-06-22 19:49:27 -0400

    added RoutineExerciseViewModel

--- app/src/main/java/com/deto/staystrong/ui/routineExercise/RoutineScreen.kt
+++ app/src/main/java/com/deto/staystrong/ui/routineExercise/RoutineScreen.kt
@@ -1,29 +1,43 @@
 package com.deto.staystrong.ui.routineExercise
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.deto.staystrong.ExerciseList
+import com.deto.staystrong.data.RoutineExercise
 import com.deto.staystrong.ui.AppViewModelProvider
+import com.deto.staystrong.ui.components.CustomCircularProgressIndicator
 import com.deto.staystrong.ui.components.CustomFloatingActionButton
 import com.deto.staystrong.ui.components.CustomTopAppBar
-import com.deto.staystrong.ui.routine.RoutinesViewModel
+import com.deto.staystrong.ui.exercise.rememberExerciseImagePainter
+
 
 @Composable
-fun RoutineScreen( navController: NavController, idRoutine: Int , viewModel: RoutinesViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
-    var exercises by remember { mutableStateOf(listOf<Exercise>()) }
+fun RoutineScreen( navController: NavController, idRoutine: Int , formattedDate: String, viewModel: RoutineExerciseViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
+
+    LaunchedEffect(Unit) {
+        viewModel.refreshRoutineExercise(idRoutine)
+    }
+
+    val uiState = viewModel.routineExerciseUiState
 
     Scaffold(
         topBar = {
@@ -51,7 +65,7 @@ fun RoutineScreen( navController: NavController, idRoutine: Int , viewModel: Rou
             ) {
 
                 Text(
-                    text = "Rutina de Hoy",
+                    text = "Rutina de $formattedDate",
                     color = Color.White,
                     fontWeight = FontWeight.Bold,
                     fontSize = 24.sp,
@@ -61,39 +75,92 @@ fun RoutineScreen( navController: NavController, idRoutine: Int , viewModel: Rou
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            LazyColumn(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(10.dp)
-            ) {
-                items(exercises) { exercise ->
-                    ExerciseItem(exercise)
+            when (uiState) {
+                is RoutineExerciseUiState.Loading -> {
+                    CustomCircularProgressIndicator("rutina")
                 }
+
+                is RoutineExerciseUiState.Error -> {
+                    Text(text = "Error: ${uiState.message}")
+                }
+
+                is RoutineExerciseUiState.Success -> {
+                    val routine = uiState.routine
+                    LazyColumn(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(10.dp)
+                    ) {
+                        items(routine) { exercise ->
+                            ExerciseItem(exercise)
+                        }
+                    }
+
+                }
+
+                else -> {}
             }
+
+
 
         }
     }
 }
 
 @Composable
-fun ExerciseItem(exercise: Exercise) {
+fun ExerciseItem(routineExercise: RoutineExercise) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("Nombre: ${exercise.name}", fontWeight = FontWeight.Bold)
-            Text("Series: ${exercise.sets}")
-            Text("Repeticiones: ${exercise.reps}")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                val painter = rememberExerciseImagePainter(routineExercise.exercise.image_path)
+
+                Image(
+                    painter = painter,
+                    contentDescription = routineExercise.exercise.name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(15.dp))
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+
+                Column {
+                    Text(
+                        text = routineExercise.exercise.name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = routineExercise.exercise.description,
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
+                }
+            }
+
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Opciones"
+                )
+            }
         }
     }
 }
 
-// Modelo de datos
-data class Exercise(
-    val name: String,
-    val sets: Int,
-    val reps: Int
-)
+
+
