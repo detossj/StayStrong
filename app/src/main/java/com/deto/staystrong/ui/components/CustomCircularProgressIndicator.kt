b38ac022c12f88bfc6292a79cdf900c5898a12cf
detossj <jorgerubilarssj@gmail.com>
2025-06-22 19:22:27 -0400

    created

---
+++ app/src/main/java/com/deto/staystrong/ui/components/CustomCircularProgressIndicator.kt
@@ -0,0 +1,29 @@
+package com.deto.staystrong.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomCircularProgressIndicator(message: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator()
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Cargando $message...")
+        }
+    }
+}
