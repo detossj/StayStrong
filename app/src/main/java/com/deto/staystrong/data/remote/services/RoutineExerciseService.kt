29737b26552a4670e999de7166ffd42ee7b785e4
detossj <jorgerubilarssj@gmail.com>
2025-06-22 19:30:26 -0400

    added interface RoutineExerciseService

---
+++ app/src/main/java/com/deto/staystrong/data/remote/services/RoutineExerciseService.kt
@@ -0,0 +1,18 @@
+package com.deto.staystrong.data.remote.services
+
+
+
+import com.deto.staystrong.data.RoutineExercise
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface RoutineExerciseService {
+
+    @POST("api/routines/{routine}/exercises")
+    suspend fun addRoutineExercise(@Path("routine") routineId: Int, @Body routineExercise: RoutineExercise): RoutineExercise
+
+    @GET("api/routines/{routine}/exercises")
+    suspend fun getRoutineExerciseById(@Path("routine") routineId: Int): List<RoutineExercise>
+}
