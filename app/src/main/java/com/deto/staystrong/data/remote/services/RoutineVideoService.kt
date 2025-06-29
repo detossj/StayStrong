02e8dcd7df94960ff480b5130e15cacc31a9c704
detossj <jorgerubilarssj@gmail.com>
2025-06-29 18:42:02 -0400

    added getRoutineVideos

--- app/src/main/java/com/deto/staystrong/data/remote/services/RoutineVideoService.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/services/RoutineVideoService.kt
@@ -1,4 +1,11 @@
 package com.deto.staystrong.data.remote.services
 
+
+import com.deto.staystrong.model.RoutineVideo
+import retrofit2.http.GET
+
 interface RoutineVideoService {
+
+    @GET("api/videos/random")
+    suspend fun getRoutineVideos(): List<RoutineVideo>
 }
