1a0ad59c8afef3861e29bbc0b5648779bf1efedd
detossj <jorgerubilarssj@gmail.com>
2025-06-30 21:12:21 -0400

    changed BASE_URL

--- app/src/main/java/com/deto/staystrong/data/remote/ApiClient.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    //private const val BASE_URL = "http://10.0.2.2:8000"
+    //const val BASE_URL = "http://10.0.2.2:8000"
     const val BASE_URL = "http://192.168.1.91:8000"
 
     fun create(context: Context): Retrofit {
