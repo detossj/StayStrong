22c7e3197d081883c769f027c468d669cfd37c2e
detossj <jorgerubilarssj@gmail.com>
2025-06-30 23:20:31 -0400

    added functions

--- app/src/main/java/com/deto/staystrong/data/remote/services/RecipeService.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/services/RecipeService.kt
@@ -1,4 +1,13 @@
 package com.deto.staystrong.data.remote.services
 
+import com.deto.staystrong.model.Recipe
+import retrofit2.http.GET
+
 interface RecipeService {
+
+    @GET("api/recipes")
+    suspend fun getRecipes(): List<Recipe>
+
+    @GET("api/recipes/{id}")
+    suspend fun getRecipesById(id: Int): Recipe
 }
