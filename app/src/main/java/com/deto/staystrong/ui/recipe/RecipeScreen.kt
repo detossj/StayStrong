9f08b57b6088a864c6a240ee56e337ae9420da44
detossj <jorgerubilarssj@gmail.com>
2025-06-30 23:22:46 -0400

    added RecipeScreen and RecipesScreen

--- app/src/main/java/com/deto/staystrong/ui/recipe/RecipeScreen.kt
+++ app/src/main/java/com/deto/staystrong/ui/recipe/RecipeScreen.kt
@@ -1,2 +1,12 @@
 package com.deto.staystrong.ui.recipe
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.deto.staystrong.ui.AppViewModelProvider
+
+
+@Composable
+fun RecipeScreen(navController : NavController) {
+
+}
--- app/src/main/java/com/deto/staystrong/ui/recipe/RecipesScreen.kt
+++ app/src/main/java/com/deto/staystrong/ui/recipe/RecipesScreen.kt
@@ -1,2 +1,17 @@
 package com.deto.staystrong.ui.recipe
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.deto.staystrong.ui.AppViewModelProvider
+
+@Composable
+fun RecipesScreen(navController : NavController, viewModel: RecipesViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
+
+    val uiState = viewModel.recipesUiState
+
+    LaunchedEffect(Unit) {
+        viewModel.refreshRecipes()
+    }
+}
