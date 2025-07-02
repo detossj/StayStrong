6e67930aad3247894c47270b2610f766a56194ee
detossj <jorgerubilarssj@gmail.com>
2025-07-01 23:53:47 -0400

    changed getRecipesById

--- app/src/main/java/com/deto/staystrong/data/remote/services/RecipeService.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/services/RecipeService.kt
@@ -2,6 +2,7 @@ package com.deto.staystrong.data.remote.services
 
 import com.deto.staystrong.model.Recipe
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RecipeService {
 
@@ -9,5 +10,5 @@ interface RecipeService {
     suspend fun getRecipes(): List<Recipe>
 
     @GET("api/recipes/{id}")
-    suspend fun getRecipesById(id: Int): Recipe
+    suspend fun getRecipesById(@Path("id") id: Int): Recipe
 }
