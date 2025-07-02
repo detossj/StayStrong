7330731bb207695bc665ad9ee1531f5a1fc81c67
alopezm22 <alopezm@ing.ucsc.cl>
2025-07-02 16:00:12 -0400

    Agrege Ruta perfil

--- app/src/main/java/com/deto/staystrong/ui/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/deto/staystrong/ui/components/CustomBottomAppBar.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.deto.staystrong.Home
+import com.deto.staystrong.Profile
 import com.deto.staystrong.R
 import com.deto.staystrong.Recipes
 import com.deto.staystrong.Routines
@@ -40,7 +41,7 @@ fun CustomBottomAppBar(navController: NavController) {
                 Triple(R.drawable.nutrition_24px, "Recetas", Recipes),
                 Triple(R.drawable.add_circle_24px, "Entrenar", Routines),
                 Triple(R.drawable.show_chart_24px, "Progreso", Home),
-                Triple(R.drawable.account_circle_24px, "Tú", Home )
+                Triple(R.drawable.account_circle_24px, "Tú", Profile )
             )
 
 
