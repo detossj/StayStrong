335e0192a515c767f50e398b38caaf5b2523a116
detossj <jorgerubilarssj@gmail.com>
2025-07-02 17:38:34 -0400

    added class DailyReminderReceiver

---
+++ app/src/main/java/com/deto/staystrong/ui/notification/DailyReminderReceiver.kt
@@ -0,0 +1,47 @@
+package com.deto.staystrong.ui.notification
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import com.deto.staystrong.R
+
+class DailyReminderReceiver : BroadcastReceiver() {
+    @RequiresPermission(Manifest.permission.SCHEDULE_EXACT_ALARM)
+    @SuppressLint("ScheduleExactAlarm")
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            NotificationScheduler.scheduleDailyReminder(context)
+            return
+        }
+
+        val channelId = "daily_reminder_channel"
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Recordatorio diario",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("¡Hora de entrenar!")
+            .setContentText("No olvides registrar tu entrenamiento hoy.")
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(1001, notification)
+    }
+}
