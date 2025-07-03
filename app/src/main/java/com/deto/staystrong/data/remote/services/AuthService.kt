9fe6b6a33ee5e24f132685c9dccb916131089790
detossj <jorgerubilarssj@gmail.com>
2025-07-03 17:00:18 -0400

    added updateProfile

--- app/src/main/java/com/deto/staystrong/data/remote/services/AuthService.kt
+++ app/src/main/java/com/deto/staystrong/data/remote/services/AuthService.kt
@@ -4,10 +4,13 @@ import com.deto.staystrong.model.LoginRequest
 import com.deto.staystrong.model.LoginResponse
 import com.deto.staystrong.model.RegisterRequest
 import com.deto.staystrong.model.RegisterResponse
+import com.deto.staystrong.model.UpdateProfileResponse
 import com.deto.staystrong.model.User
+import com.deto.staystrong.model.UserUpdateRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthService {
@@ -22,4 +25,10 @@ interface AuthService {
 
     @POST("api/logout")
     suspend fun logout( @Header("Authorization") token: String)
+
+    @PATCH("api/profile")
+    suspend fun updateProfile(@Header("Authorization") token: String, @Body updatedUser: UserUpdateRequest): UpdateProfileResponse
 }
+
+
+
