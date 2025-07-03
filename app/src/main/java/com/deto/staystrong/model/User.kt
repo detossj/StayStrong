f8659a5ebbc27b524e92f2af50eb74e9bce96106
detossj <jorgerubilarssj@gmail.com>
2025-07-03 17:02:34 -0400

    changed User

--- app/src/main/java/com/deto/staystrong/model/User.kt
+++ app/src/main/java/com/deto/staystrong/model/User.kt
@@ -1,3 +1,13 @@
 package com.deto.staystrong.model
 
-data class User(val id: Int, val name: String, val email: String)
+data class User(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val sex: String?,
+    val birth: String?,
+    val weight: Float?,
+    val height: Float?,
+    val bio: String?,
+    val ig: String?,
+)
