18a6d5758e2479b746220f2e4cba31121576cf60
bastian <bastij58@gmail.com>
2025-07-08 14:29:30 -0400

    agregue atributo type y musclegroup

--- app/src/main/java/com/deto/staystrong/model/Routine.kt
+++ app/src/main/java/com/deto/staystrong/model/Routine.kt
@@ -4,5 +4,7 @@ package com.deto.staystrong.model
 data class Routine(
     val id: Int,
     val user_id: Int,
-    val date: String
+    val date: String,
+    val type: String,
+    val muscle_group: String?
 )
